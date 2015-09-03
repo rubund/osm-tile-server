22112cf34c4676009f05111c89ef1462a20a2510
Ruben Undheim <ruben.undheim@gmail.com>
2015-09-03 20:12:47 +0200

    Added conf-file + some other things

--- src/osm-tile-server-import.c
+++ src/osm-tile-server-import.c
@@ -3,7 +3,12 @@
 
 int main(int argc, char **argv)
 {
-	system("whoami");
+	char command[256];
+
+	snprintf(command,255,"osm2pgsql --slim -d $DBNAME --cache-strategy sparse -C 100 --hstore -S /usr/share/osm2pgsql/default.style /usr/share/osm-tile-server-base/samples/liechtenstein-latest.osm.pbf");
+	printf("Importing to database with this command:\n");
+	printf(" %s\n",command);
+	//system(command);
 
 	return 0;
 
