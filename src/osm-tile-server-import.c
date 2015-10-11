6ce6711873f19ce665efd2b7244cf876432f22a9
Ruben Undheim <ruben.undheim@gmail.com>
2015-10-11 21:05:57 +0200

    Get rid of warning

--- src/osm-tile-server-import.c
+++ src/osm-tile-server-import.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 		snprintf(cfg_slim,10,"--slim");
 	}
 	else {
-		snprintf(cfg_slim,10,"");
+		cfg_slim[0] = 0;
 	}
 	snprintf(command,255,"osm2pgsql %s -d %s --cache-strategy %s -C %s --hstore -S /usr/share/osm2pgsql/default.style %s",cfg_slim,cfg_dbname,cfg_cache_strategy,cfg_memory,pbffile);
 	printf("Importing to database with this command:\n\n");
